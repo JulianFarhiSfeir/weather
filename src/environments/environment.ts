bdcab3f03e4fffd3484b7d549c9d002ed7bcc24d
JulianFarhiSfeir <farhi.j@sfeir.com>
2023-10-17 17:11:38 +0200

    Updated environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   production: false,
-  weatherApiBaseUrl: 'http://api.openweathermap.org/data/2.5',
+  weatherApiBaseUrl: 'https://api.openweathermap.org/data/2.5',
   weatherApiId: '5a4b2d457ecbef9eb2a71e480b947604',
   weatherIconUrl: 'https://raw.githubusercontent.com/udacity/Sunshine-Version-2/sunshine_master/app/src/main/res/drawable-hdpi/',
 };

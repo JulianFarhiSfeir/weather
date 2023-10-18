7f2f8e65b36f1367b50f5d54e949bec6ccd10a9f
JulianFarhiSfeir <farhi.j@sfeir.com>
2023-10-18 17:58:53 +0200

    Refactored

--- src/app/core/api/weather-api.interceptor.ts
+++ src/app/core/api/weather-api.interceptor.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpParams, HttpRequest} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {environment} from "../../../environments/environment";
 
@@ -9,7 +9,7 @@ export class WeatherApiInterceptor implements HttpInterceptor {
 		if (!request.url.includes(environment.weatherApiBaseUrl)) {
 			return next.handle(request);
 		}
-		let params = request.params.set('APPID', environment.weatherApiId);
+		let params: HttpParams = request.params.set('APPID', environment.weatherApiId);
 		const clonedRequest = request.clone({
 			params
 		})
--- src/app/core/api/weather-api.service.ts
+++ src/app/core/api/weather-api.service.ts
@@ -1,22 +1,20 @@
-import {Injectable, Signal, signal} from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs';
-
 import {HttpClient, HttpParams} from '@angular/common/http';
 import {ExternalCurrentCondition, ExternalForecast} from './weather-api.typings';
-import {ConditionsAndZip} from "../weather-manager.typings";
 import {environment} from "../../../environments/environment";
 
 @Injectable({
 	providedIn: 'root',
 })
 export class WeatherApiService {
-	static URL = environment.weatherApiBaseUrl;
+	private static URL: string = environment.weatherApiBaseUrl;
 
 	constructor(private http: HttpClient) {
 	}
 
 	public getConditionByZipCode(zipcode: string): Observable<ExternalCurrentCondition> {
-		const params = new HttpParams({
+		const params: HttpParams = new HttpParams({
 			fromObject: {
 				zip: `${zipcode},us`,
 				units: 'imperial'
@@ -26,7 +24,7 @@ export class WeatherApiService {
 	}
 
 	public getForecast(zipcode: string): Observable<ExternalForecast> {
-		const params = new HttpParams({
+		const params: HttpParams = new HttpParams({
 			fromObject: {
 				zip: `${zipcode},us`,
 				units: 'imperial',
